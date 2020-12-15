ca79d915b7900cf6fb3155cd7bb6ff6ebf20b431
simon <simon@nlplab.cc>
2020-12-15 21:19:13 +0800

    [add] add flag 'z' to debug.h

--- lib/debug.h
+++ lib/debug.h
@@ -30,6 +30,7 @@ const char dbgAddr = 'a'; 		// address spaces
 const char dbgNet = 'n'; 		// network emulation
 const char dbgSys = 'u';                // systemcall
 const char dbgTraCode = 'c';
+const char dbgSchedule = 'z';
 
 class Debug {
   public:
